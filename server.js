51fcc71cf2595de12215ccb90befeaba47bb1283
Danny <223181@ids.upchiapas.edu.mx>
2025-05-14 13:30:55 -0600

    Change file jenkinsfile

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ const app = express();
 app.use(helmet());
 app.use(express.json());
 
-app.get('/health', (_req, res) => {
-    res.status(200).json({
-        status: 'UP',
-        timestamp: new Date().toISOString(),
-        author: 'Gerson Daniel '
-    });
+app.get('/healthcheck', (_req, res) => {
+  res.status(200).json({ status: 'prod', timestamp: new Date().toISOString() });
+});
+
+app.get('/names', (_req, res) => {
+  res.status(200).json({ author: 'Gerson daniel' });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Healthcheck service running on port ${PORT}`);
+  console.log(`Healthcheck service running on port ${PORT}`);
 });
