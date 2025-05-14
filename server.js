a02530ccc0579a2c1eb36a2b7a096c61762ed84e
Danny <223181@ids.upchiapas.edu.mx>
2025-05-13 23:44:00 -0600

    changes file server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(helmet());
 app.use(express.json());
 
 app.get('/health', (_req, res) => {
-  res.status(200).json({ status: 'UP', timestamp: new Date().toISOString(), author: 'Gerson Daniel Garcia Dominguez' });
+  res.status(200).json({ status: 'UP', timestamp: new Date().toISOString(), author: 'Gerson Daniel Garcia Dominguez / Danny' });
 });
 
 const PORT = process.env.PORT || 3000;
